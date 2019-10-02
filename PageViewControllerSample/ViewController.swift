b4b9c1df70e6c8c69f1ba6323d53c848c52f2a1c
kvoyant <kvoyant@gmail.com>
2019-10-02 21:47:18 +0900

    pageControl autolayout 만 설정

--- PageViewControllerSample/ViewController.swift
+++ PageViewControllerSample/ViewController.swift
@@ -1,20 +1,12 @@
-//
-//  ViewController.swift
-//  PageViewControllerSample
-//
-//  Created by APPLE on 02/10/2019.
-//  Copyright © 2019 yhkim. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pageControl: UIPageControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
